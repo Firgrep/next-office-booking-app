dc4d2d9484dddb338a5c7a9c1a836cf476b93326
Firgrep <awdeegrays@gmail.com>
2023-05-24 00:26:37 +0200

    back to docker

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: "standalone",
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
    * out.
